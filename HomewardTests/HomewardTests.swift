785188ba6996508ba2f7f518e53d09c14d2418c9
John Pavley <john@pavley.com>
2015-01-24 15:11:21 -0500

    Initial Commit

---
+++ HomewardTests/HomewardTests.swift
@@ -0,0 +1,36 @@
+//
+//  HomewardTests.swift
+//  HomewardTests
+//
+//  Created by John Pavley (SSD) on 1/24/15.
+//  Copyright (c) 2015 John F Pavley. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class HomewardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
