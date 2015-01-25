b4c33b2182a5c33e8da98515b0a44f2e3076c208
John Pavley <john@pavley.com>
2015-01-25 11:30:02 -0500

    move code from viewDidLoad to viewDidAppear

--- Homeward/ViewController.swift
+++ Homeward/ViewController.swift
@@ -16,8 +16,19 @@ class ViewController: UIViewController {
     private let kErrorHomewardContactMissing   = 3
     
     override func viewDidLoad() {
+        
+        // NOTE: viewDidLoad is usally called once, when the view controller is loaded into memory and the view is not yet visible. The view itself is not usable at this point. What the user sees at this point is the launch screen.
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        // NOTE: viewDidAppear is called after the view is visible and will be called multiple times while the app is running and view is visible.  What the user sees at this point is the view.
+        
+        // QUESTION: This code makes sure the address book exists, the user is allowing the app to access it, the contact "Homeward" exists, and then kicks off the actuall work of the app. Not all of this code needs be run more than once... or does it? What if the user deletes the contact app? Changes their mind about access? Updates their home address?
         
         let addressBook: ABAddressBookRef? = getRefAddressBook()
         
@@ -38,7 +49,7 @@ class ViewController: UIViewController {
                     println("The name of this contact is \(contactName)")
                     
                 } else {
-    
+                    
                     displayErrorAlert(kErrorNotOkToAccessAddressBook, tryAgain: true)
                 }
                 
@@ -123,6 +134,13 @@ class ViewController: UIViewController {
     }
     
     private func displayErrorAlert(errorID: Int, tryAgain: Bool) {
+        
+        let alert = UIAlertController(title: title, message: "Important Message", preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Try Again", style: .Default, handler: nil)
+        
+        alert.addAction(action)
+        //presentViewController(alert, animated: true, completion: nil)
+
         
     }
 }
